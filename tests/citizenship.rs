963b65d66b4701cb43214873a50fc3fe2ed8fc52
Esteban Borai <estebanborai@gmail.com>
2020-08-01 01:20:25 -0300

    ci: setup tests for ci

---
+++ tests/citizenship.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod tests {
+  use libcne::cne::Citizenship;
+
+  #[test]
+  fn converts_V_to_string() {
+    let have = Citizenship::V;
+    let want = "V".to_string();
+
+    assert_eq!(have, want);
+  }
+
+  #[test]
+  fn converts_E_to_string() {
+    let have = Citizenship::E;
+    let want = "E".to_string();
+
+    assert_eq!(have, want);
+  }
+}
